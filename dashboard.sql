bd854c75a60293f7409076ceea8e7e54c96c3332
Ivkina Sofya <145347304+Sofushka73@users.noreply.github.com>
2024-01-18 16:06:46 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,3 +1,4 @@
+--dashboard utm_all
 select utm_campaign, utm_source, utm_medium,
 round(sum(total_cost)/sum(visitors_count)) as cpu,
 case 
@@ -58,3 +59,110 @@ group by s.visitor_id, l.lead_id, visit_date, utm_campaign, utm_source, utm_medi
 where utm_campaign is not null
 group by utm_campaign, utm_source, utm_medium
 
+
+--dashboard day/week/month count
+select
+ utm_source, utm_medium, utm_campaign,
+date_trunc('month', visit_date) as visit_date,
+count(distinct visitor_id)
+from sessions s 
+left join vk_ads va 
+on s.source = va.utm_source 
+and s.medium = va.utm_medium 
+and s.campaign = va.utm_campaign 
+where utm_source is not null
+group by 4, 1, 2,3
+
+ union 
+ 
+select
+ utm_source, utm_medium, utm_campaign,
+date_trunc('month', visit_date) as visit_date,
+count(distinct visitor_id)
+from sessions s 
+left join ya_ads ya  
+on s.source = ya.utm_source 
+and s.medium = ya.utm_medium 
+and s.campaign = ya.utm_campaign 
+where utm_source is not null
+group by 4, 1, 2,3
+
+--dashboard conversion
+select 
+(count(distinct lead_id) * 100.0)/count(distinct s.visitor_id) as conversion 
+from sessions s
+left join leads l 
+on s.visitor_id = l.visitor_id 
+
+union
+
+select (leads_count * 100.0) /lead_count  as conversion
+from (select count(distinct lead_id) as lead_count,
+count(distinct lead_id) filter(where status_id = '142') as leads_count
+from leads ) as tab
+
+--dashboard profit
+select utm_source, utm_medium, utm_campaign,
+revenue - total_cost as profit
+from (select
+utm_source, utm_medium, utm_campaign,
+sum (va.daily_spent) as total_cost,
+sum(amount) as revenue
+from sessions s 
+left join vk_ads va 
+on s.source = va.utm_source 
+and s.medium = va.utm_medium 
+and s.campaign = va.utm_campaign 
+left join leads l 
+on s.visitor_id  = l.visitor_id 
+where utm_source is not null
+group by 1, 2,3) as tab
+
+union
+
+select utm_source, utm_medium, utm_campaign,
+revenue - total_cost as profit
+from(select
+utm_source, utm_medium, utm_campaign,
+sum (ya.daily_spent) as total_cost,
+sum(amount) as revenue
+from sessions s 
+left join ya_ads ya  
+on s.source = ya.utm_source 
+and s.medium = ya.utm_medium 
+and s.campaign = ya.utm_campaign 
+left join leads l 
+on s.visitor_id  = l.visitor_id 
+where utm_source is not null
+group by  1, 2,3) as tab
+
+--dashboard day/week/month total cost
+select utm_source, utm_medium, utm_campaign,
+visit_date, total_cost
+from (select
+utm_source, utm_medium, utm_campaign,
+to_char((date_trunc('month', visit_date)), 'yyyy-mm-dd') as visit_date,
+sum (va.daily_spent) as total_cost
+from sessions s 
+left join vk_ads va 
+on s.source = va.utm_source 
+and s.medium = va.utm_medium 
+and s.campaign = va.utm_campaign  
+where utm_source is not null
+group by 1, 2,3) as tab
+
+union
+
+select utm_source, utm_medium, utm_campaign,
+visit_date, total_cost
+from(select
+utm_source, utm_medium, utm_campaign,
+to_char((date_trunc('month', visit_date)), 'yyyy-mm-dd') as visit_date,
+sum (ya.daily_spent) as total_cost
+from sessions s 
+left join ya_ads ya  
+on s.source = ya.utm_source 
+and s.medium = ya.utm_medium 
+and s.campaign = ya.utm_campaign 
+where utm_source is not null
+group by  1, 2,3) as tab
