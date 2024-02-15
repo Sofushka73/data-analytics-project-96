84b6c911d0d94f5d7b72ee55d00ed1f7bea8b315
Ivkina Sofya <145347304+Sofushka73@users.noreply.github.com>
2024-02-15 16:09:29 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -5,21 +5,22 @@ select
     l.closing_reason, l.status_id
 from
     (
-    select distinct on (s.visitor_id) s.visitor_id,
-        s.visit_date, s.source, s.medium,
-        s.campaign
-    from sessions as s
-    where
-        s.medium = 'cpc' or s.medium = 'cpm'
+        select distinct on (s.visitor_id) s.visitor_id,
+            s.visit_date, s.source, s.medium,
+            s.campaign
+        from sessions as s
+        where
+            s.medium = 'cpc' or s.medium = 'cpm'
             or s.medium = 'cpa' or s.medium = 'youtube'
             or s.medium = 'cpp' or s.medium = 'tg'
             or s.medium = 'social'
-    order by s.visitor_id, s.visit_date desc
+        order by s.visitor_id, s.visit_date desc
     ) as tabb
 left join leads as l
     on
         tabb.visitor_id = l.visitor_id
-group by tabb.visitor_id, tabb.visit_date, utm_source, utm_medium, utm_campaign,
+group by
+    tabb.visitor_id, tabb.visit_date, utm_source, utm_medium, utm_campaign,
     l.lead_id, l.created_at, l.closing_reason, l.status_id
 order by
     amount desc nulls last,
