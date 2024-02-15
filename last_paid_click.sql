a3f7d6f3aef6c0cba6cdef96d0c3b8e3a53fd889
Ivkina Sofya <145347304+Sofushka73@users.noreply.github.com>
2024-02-15 15:46:28 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,23 +1,23 @@
-select tabb.visitor_id, visit_date, source as utm_source,
-    medium as utm_medium, campaign as utm_campaign,
-    lead_id, created_at, sum(amount) as amount,
-    closing_reason, status_id
+select    tabb.visitor_id, tabb.visit_date, tabb.source as utm_source,
+    tabb.medium as utm_medium, tabb.campaign as utm_campaign,
+    l.lead_id, l.created_at, sum(l.amount) as amount,
+    l.closing_reason, l.status_id
 from
-   (select distinct on(visitor_id) visitor_id,
-       s.visit_date, s.source, s.medium,
-       s.campaign 
-   from sessions as s 
-   where medium = 'cpc' or medium = 'cpm'
-   or medium = 'cpa' or medium = 'youtube'
-   or medium = 'cpp' or medium = 'tg'
-   or medium = 'social'
-   order by visitor_id, visit_date desc
-   ) as tabb 
-   left join leads as l 
-      on tabb.visitor_id = l.visitor_id 
-group by tabb.visitor_id, visit_date, utm_source, utm_medium, utm_campaign,
-    lead_id, created_at, closing_reason, status_id
-order by amount desc nulls last,
-   visit_date asc, utm_source asc,
-   utm_medium asc, utm_campaign asc
+    (        select distinct on (s.visitor_id) s.visitor_id,
+        s.visit_date, s.source, s.medium,
+        s.campaign
+    from sessions as s
+    where        s.medium = 'cpc' or s.medium = 'cpm'
+            or s.medium = 'cpa' or s.medium = 'youtube'
+            or s.medium = 'cpp' or s.medium = 'tg'
+            or s.medium = 'social'
+    order by s.visitor_id, s.visit_date desc
+    ) as tabb
+left join leads as l
+    on tabb.visitor_id = l.visitor_id
+group by tabb.visitor_id, tabb.visit_date, utm_source, utm_medium, utm_campaign,
+    l.lead_id, l.created_at, l.closing_reason, l.status_id
+order by    amount desc nulls last,
+    tabb.visit_date asc, utm_source asc,
+    utm_medium asc, utm_campaign asc
 limit 10
