7e8316284dc3d86df914601e03d20d86a73b2c11
Ivkina Sofya <145347304+Sofushka73@users.noreply.github.com>
2024-02-15 14:54:16 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,53 +1,23 @@
-select
-   tabb.visitor_id,
-   visit_date,
-   source as utm_source,
-   medium as utm_medium,
-   campaign as utm_campaign,
-   lead_id,
-   created_at,
-   sum(amount) as amount,
-   closing_reason,
-   status_id 
+select tabb.visitor_id, visit_date, source as utm_source,
+    medium as utm_medium, campaign as utm_campaign,
+    lead_id, created_at, sum(amount) as amount,
+    closing_reason, status_id
 from
-   (
-      select distinct
-         on(visitor_id) visitor_id,
-         s.visit_date,
-         s.source,
-         s.medium,
-         s.campaign 
-      from
-         sessions s 
-      where
-         medium = 'cpc' 
-         or medium = 'cpm' 
-         or medium = 'cpa' 
-         or medium = 'youtube' 
-         or medium = 'cpp' 
-         or medium = 'tg'
-         or medium = 'social' 
-      order by
-         visitor_id,
-         visit_date desc
-   )
-   as tabb 
-   left join
-      leads l 
+   (select distinct on(visitor_id) visitor_id,
+       s.visit_date, s.source, s.medium,
+       s.campaign 
+   from sessions as s 
+   where medium = 'cpc' or medium = 'cpm'
+   or medium = 'cpa' or medium = 'youtube'
+   or medium = 'cpp' or medium = 'tg'
+   or medium = 'social'
+   order by visitor_id, visit_date desc
+   ) as tabb 
+   left join leads as l 
       on tabb.visitor_id = l.visitor_id 
-group by
-   1,
-   2,
-   3,
-   4,
-   5,
-   6,
-   7,
-   9,
-   10 
-order by
-   amount desc nulls last,
-   visit_date asc,
-   utm_source asc,
-   utm_medium asc,
-   utm_campaign asc limit 10
+group by tabb.visitor_id, visit_date, utm_source, utm_medium, utm_campaign,
+    lead_id, created_at, closing_reason, status_id
+order by amount desc nulls last,
+   visit_date asc, utm_source asc,
+   utm_medium asc, utm_campaign asc
+limit 10
