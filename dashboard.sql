d4e9c29aa0307e588073b3cc807ccd34eedb0282
Ivkina Sofya <145347304+Sofushka73@users.noreply.github.com>
2024-03-14 18:07:33 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -6,7 +6,7 @@ select
     utm_medium,
     round(
         sum(total_cost)
-    / sum(visitors_count)
+        / sum(visitors_count)
     ) as cpu,
     case
         when sum(leads_count) = 0 then 0
@@ -19,14 +19,14 @@ select
     round(((
         sum(revenue) - sum(total_cost)
     )
-	/ sum(total_cost)
+    / sum(total_cost)
     ) * 100
     ) as roi
 from (
     select
         va.utm_campaign,
-	va.utm_source,
-	va.utm_medium,
+        va.utm_source,
+        va.utm_medium,
         count(s.visitor_id) over (partition by s.visit_date) as visitors_count,
         sum(va.daily_spent) as total_cost,
         count(l.lead_id) over (partition by s.visit_date) as leads_count,
@@ -54,28 +54,28 @@ select
     utm_medium,
     round(
         sum(total_cost)
-    / sum(visitors_count)
+        / sum(visitors_count)
     ) as cpu,
     case
         when sum(leads_count) = 0 then 0
-	else round(sum(total_cost) / sum(leads_count))
+        else round(sum(total_cost) / sum(leads_count))
     end as cpi,
     case
-	when sum(purchases_count) = 0 then 0
-	else round(sum(total_cost) / sum(purchases_count))
+        when sum(purchases_count) = 0 then 0
+        else round(sum(total_cost) / sum(purchases_count))
     end as cppu,
     round(((sum(revenue) - sum(total_cost)) / sum(total_cost)) * 100) as roi
 from (
     select
         ya.utm_campaign,
-	ya.utm_source,
-	ya.utm_medium,
+        ya.utm_source,
+        ya.utm_medium,
         count(s.visitor_id) over (partition by s.visit_date)
-	as visitors_count,
+        as visitors_count,
         sum(ya.daily_spent) as total_cost,
         count(l.lead_id) over (partition by s.visit_date) as leads_count,
         count(l.lead_id) filter (where l.status_id = 142)
-	as purchases_count,
+        as purchases_count,
         sum(l.amount) filter (where l.status_id = 142) as revenue
     from sessions as s
     left join ya_ads as ya
@@ -92,8 +92,8 @@ from (
 ) as tab
 where utm_campaign is not null
 group by
-    utm_campaign, utm_source, utm_medium
-;
+    utm_campaign, utm_source, utm_medium;
+
 --dashboard day/week/month count
 
 select
@@ -105,7 +105,7 @@ select
 from sessions as s
 left join vk_ads as va
     on
-	s.source = va.utm_source
+        s.source = va.utm_source
         and s.medium = va.utm_medium
         and s.campaign = va.utm_campaign
 where va.utm_source is not null
@@ -128,30 +128,9 @@ left join ya_ads as ya
 where ya.utm_source is not null
 group by
     date_trunc('month', s.visit_date), ya.utm_source, ya.utm_medium,
-    ya.utm_campaign
-;
---dashboard conversion
+    ya.utm_campaign;
 
-select
-    (count(distinct l.lead_id) * 100.0)
-    / count(distinct s.visitor_id) as conversion
-from sessions as s
-left join leads as l
-    on
-	s.visitor_id = l.visitor_id
-union
-select
-    (tabb.leads_count * 100.0)
-    / tabb.lead_count as conversionn
-from (
-    select
-        count(distinct l.lead_id) as lead_count,
-        count(distinct l.lead_id) filter (where l.status_id = '142')
-	as leads_count
-    from leads
-) as tabb
-;
---dashboard profit
+--dashboard conversion
 
 select
     tab.utm_source,
@@ -162,8 +141,8 @@ select
 from (
     select
         va.utm_source,
-	va.utm_medium,
-	va.utm_campaign,
+        va.utm_medium,
+        va.utm_campaign,
         sum(va.daily_spent) as total_cost,
         sum(l.amount) as revenue
     from sessions as s
@@ -189,8 +168,8 @@ select
 from (
     select
         ya.utm_source,
-	ya.utm_medium,
-	ya.utm_campaign,
+        ya.utm_medium,
+        ya.utm_campaign,
         sum(ya.daily_spent) as total_cost,
         sum(l.amount) as revenue
     from sessions as s
@@ -205,8 +184,8 @@ from (
     where ya.utm_source is not null
     group by
         ya.utm_source, ya.utm_medium, ya.utm_campaign
-) as tab
-;
+) as tab;
+
 --dashboard day/week/month total cost
 
 select
@@ -218,15 +197,15 @@ select
 from (
     select
         va.utm_source,
-	va.utm_medium,
-	va.utm_campaign,
+        va.utm_medium,
+        va.utm_campaign,
         to_char((date_trunc('month', s.visit_date)), 'yyyy-mm-dd')
-	as visit_date,
+        as visit_date,
         sum(va.daily_spent) as total_cost
     from sessions as s
     left join vk_ads as va
         on
-	    s.source = va.utm_source
+            s.source = va.utm_source
             and s.medium = va.utm_medium
             and s.campaign = va.utm_campaign
     where va.utm_source is not null
@@ -243,26 +222,25 @@ select
 from (
     select
         ya.utm_source,
-	ya.utm_medium,
-	ya.utm_campaign,
+        ya.utm_medium,
+        ya.utm_campaign,
         to_char((date_trunc('month', s.visit_date)), 'yyyy-mm-dd')
-	as visit_date,
+        as visit_date,
         sum(ya.daily_spent) as total_cost
     from sessions as s
     left join ya_ads as ya
         on
-	    s.source = ya.utm_source
+            s.source = ya.utm_source
             and s.medium = ya.utm_medium
             and s.campaign = ya.utm_campaign
     where ya.utm_source is not null
     group by
         ya.utm_source, ya.utm_medium, ya.utm_campaign
-) as tab
-;
+) as tab;
 
 --Вычисления для выявлелия кореляции между запуском рекламы и ростом органики
-select
-    count(distinct s.visitor_id)
+
+select count(distinct s.visitor_id)
 from sessions as s
 left join vk_ads as va
     on
@@ -271,72 +249,70 @@ left join vk_ads as va
         and s.campaign = va.utm_campaign
 left join leads as l
     on
-	s.visitor_id = l.visitor_id
-where va.utm_source is null and l.created_at <= '2023-06-30 18:28:25.000'
-;
+        s.visitor_id = l.visitor_id
+where va.utm_source is null and l.created_at <= '2023-06-30 18:28:25.000';
 -- created_at <= '2023-06-01 01:58:59.000'
 
-select
-    max(created_at)
+select max(created_at)
 
 --min(created_at)
 
 from (
     select distinct
-	s.visitor_id,
-	l.lead_id,
+        s.visitor_id,
+        l.lead_id,
         s.visit_date,
-	l.created_at,
+        l.created_at,
         l.status_id,
-	l.created_at - s.visit_date as time
+        l.created_at - s.visit_date as time
     from sessions as s
     left join leads as l
         on
-	    s.visitor_id = l.visitor_id
+            s.visitor_id = l.visitor_id
     where l.status_id = '142'
     order by s.visit_date asc, l.created_at asc
-) as tab
-;
+) as tab;
+
 --Вычисление времени когда можно начинать анализ дашборда
 
 select avg(time)
 from (
     select distinct
-	s.visitor_id,
+        s.visitor_id,
         l.lead_id,
-	s.visit_date,
+        s.visit_date,
         l.created_at,
-	l.status_id,
+        l.status_id,
         l.created_at - s.visit_date as time
     from sessions as s
     left join leads as l
         on
-	    s.visitor_id = l.visitor_id
-            and l.created_at > s.visit_date
-      where l.status_id = '142'
-      order by s.visit_date asc, l.created_at asc
-) as tab
-;
+            s.visitor_id = l.visitor_id
+            and s.visit_date < l.created_at
+    where l.status_id = '142'
+    order by s.visit_date asc, l.created_at asc
+) as tab;
+
 --вычисления где вычислялся промежуток покрытия 90%
 
 --select min/max(time)
 
-select
-    count(visitor_id) 
+select count(visitor_id)
 from (
     select distinct
-	s.visitor_id,
-	l.lead_id,
+        s.visitor_id,
+        l.lead_id,
         s.visit_date,
-	l.created_at,
-	l.status_id,
+        l.created_at,
+        l.status_id,
         l.created_at - s.visit_date as time
     from sessions as s
     left join leads as l
         on
-	    s.visitor_id = l.visitor_id
-            and l.created_at > s.visit_date
+            s.visitor_id = l.visitor_id
+            and s.visit_date < l.created_at
     where l.status_id = '142'
     order by s.visit_date asc, l.created_at asc
-    ) as tab
+) as tab
 where time between '00:17:16.678171' and '23 days 09:25:25'
+
